c0a5b67968d4c69f1d7db2b330dd778e7bf2b743
Kellen Pugmire <kepugmire@gmail.com>
2017-08-31 20:51:48 -0600

    finished

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,9 +1,90 @@
 module.exports = {
 
-
-
-
-
-
+    getUsers: (req, res) => {
+        req.app.get('db').get_users().then(response => {
+            res.send(response)
+        })
+    },
+    getVehicles: (req, res) => {
+        req.app.get('db').get_vehicles().then(response => {
+            res.send(response)
+        })
+    },
+    getVehiclesByYear: (req, res) => {
+        req.app.get('db').get_vehicles_by_year().then(response => {
+            res.send(response)
+        })
+    },
+    addUser: (req, res) => {
+        const newUser = [
+            req.body.name,
+            req.body.email
+        ]
+        req.app.get('db').add_user(newUser).then(response => {
+            res.send(response)
+        })
+    },
+    addVehicle: (req, res) => {
+        const newVehicle = [
+            req.body.make,
+            req.body.model,
+            req.body.year,
+            req.body.owner_id
+        ]
+        req.app.get('db').add_vehicle(newVehicle).then(response => {
+            res.send(response)
+        })
+    },
+    getVehicleCount: (req, res) => {
+        const count = [
+            req.params.userId
+        ]
+        req.app.get('db').vehicle_count(count).then(response => {
+            res.send(response)
+        })
+    },
+    getVehicleById: (req, res) => {
+        const id = [
+            req.params.userId
+        ]
+        req.app.get('db').vehicle_id(id).then(response => {
+            res.send(response)
+        })
+    },
+    findAllVehicles: (req, res) => {
+        if (req.query.userEmail) {
+            req.app.get('db').find_vehicle_email([req.query.userEmail]).then(response => {
+                res.send(response)
+            })
+        } else if (req.query.userFirstStart) {
+            const first = req.query.userFirstStart;
+            req.app.get('db').find_vehicle_first_name([first + '%']).then(resp => {
+                res.send(resp)
+            })
+        }
+    },
+    changeOwner: (req, res) => {
+        const change = [
+            Number(req.params.vehicleId),
+            Number(req.params.userId)
+        ]
+        req.app.get('db').change_owner(change).then(response => {
+            res.send(response)
+        })
+    },
+    removeOwner: (req, res) => {
+        const owner = [
+            req.params.userId,
+            req.params.vehicleId
+        ]
+        req.app.get('db').remove_owner(owner).then(response => {
+            res.send(response)
+        })
+    },
+    removeVehicle: (req, res) => {
+        req.app.get('db').remove_vehicle([req.params.vehicleId]).then(response => {
+            res.send(response)
+        })
+    }
 
 }
--- server.js
+++ server.js
@@ -12,13 +12,8 @@ app.use(cors());
 
 // You need to complete the information below to connect
 // to the assessbox database on your postgres server.
-massive({
-  host: //host,
-  port: //port,
-  database: //database,
-  user: //user,
-  password: //password
-}).then( db => {
+
+massive('postgres://postgres:@localhost/assessbox').then(db => {
   app.set('db', db);
 
   // Initialize user table and vehicle table.
@@ -34,9 +29,22 @@ massive({
 
 // ===== Build enpoints below ============
 
+app.get('/api/users', mainCtrl.getUsers);
+app.get('/api/vehicles', mainCtrl.getVehicles);
+
+app.post('/api/users', mainCtrl.addUser);
+app.post('/api/vehicles', mainCtrl.addVehicle);
+
+app.get('/api/user/:userId/vehiclecount', mainCtrl.getVehicleCount);
+app.get('/api/user/:userId/vehicle', mainCtrl.getVehicleById);
+app.get('/api/vehicles', mainCtrl.findAllVehicles);
+app.get('/api/newervehiclesbyyear', mainCtrl.getVehiclesByYear);
 
 
+app.put('/api/vehicle/:vehicleId/user/:userId', mainCtrl.changeOwner);
 
+app.delete('/api/user/:userId/vehicle/:vehicleId', mainCtrl.removeOwner);
+app.delete('/api/vehicle/:vehicleId', mainCtrl.removeVehicle);
 
 
 
